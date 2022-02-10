9c97d284f8a89ed7fdd6aee0da9d332cf025c7dc
Elliot Su <sodinfeliz@gmail.com>
2022-02-10 09:57:24 +0800

    Update `update_database` method.

--- main.py
+++ main.py
@@ -1,38 +1,53 @@
 import requests, json
+from tqdm import tqdm
+from typing import Dict, Union
 
 
 json_fn = "database"
 
 
-def read_databae(database_id, headers):
+def read_databae(database_id, headers, save=True) -> Union[Dict, None]:
     url = f"https://api.notion.com/v1/databases/{database_id}/query"
-    payload = {"page_size": 100}
+    payload = {"page_size": 1000}
     res = requests.request("POST", url, json=payload, headers=headers)
     data = res.json()
+    print(len(data['results']))
 
-    with open(f'./{json_fn}.json', 'w', encoding='utf8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
+    if save:
+        with open(f'./{json_fn}.json', 'w', encoding='utf8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+    else:
+        return data
 
 
 def update_database(database_id, headers):
-    url = f"https://api.notion.com/v1/databases/{database_id}"
+    
+    db_data = read_databae(database_id, headers, save=False)
+    pages = db_data['results']
 
-    with open(f'{json_fn}.json', encoding="utf-8") as json_file:
-        data = json.load(json_file) 
-
-    for page in data['results']:
-        page['icon'] = {'type': 'external', 'external': {'url': 'https://raw.githubusercontent.com/eirikmadland/notion-icons/master/v5/icon3/mi-basket-ball.svg'}}
-    data = json.dumps(data)
-    response = requests.request("PATCH", url, headers=headers, data=data)
-
-    print(response.text)
+    with tqdm(total=len(pages), desc='Updating') as pbar:
+        for page in pages:
+            page_url = f"https://api.notion.com/v1/pages/{page['id']}"
+            payload = {
+                "icon": {
+                    "type": "external",
+                    "external": {
+                        "url": "https://raw.githubusercontent.com/eirikmadland/notion-icons/master/v5/icon3/ul-label-alt.svg"
+                    }
+                }
+            }
+            response = requests.request("PATCH", page_url, json=payload, headers=headers)
+            if response.status_code != 200:
+                print(f"Error updating page {page['id']}")
+            
+            pbar.update(1)
 
 
 if __name__ == "__main__":
     with open('token.txt', encoding='utf8') as f:
         api_token = f.readline()
     
-    database_id = "54806c81a3234dc899af940be2dbdf53"
+    database_id = "213bac35fee64ef3b67a1c4e40ab7a4b"
 
     headers = {
         "Accept": "application/json",
@@ -41,5 +56,4 @@ if __name__ == "__main__":
         "Authorization": f"Bearer {api_token}"
     }
 
-    read_databae(database_id, headers)
-    #update_database(database_id, headers)
+    update_database(database_id, headers)
